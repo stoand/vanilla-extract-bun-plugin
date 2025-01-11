27cfa7e574b61cc1f5ccc84352d618a17d7c2c1b
Andreas Stocker <stoand@protonmail.com>
2025-01-11 13:25:30 +0100

    plugin: add 2 sections

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-import { type BunPlugin } from "bun";
+import { type BunPlugin } from 'bun';
+import { dirname, join } from 'path';
+
+import {
+    cssFileFilter,
+    virtualCssFileFilter,
+    processVanillaFile,
+    getSourceFromVirtualCssFile,
+    compile,
+    vanillaExtractTransformPlugin,
+    type IdentifierOption,
+    type CompileOptions,
+} from '@vanilla-extract/integration';
+
+const vanillaCssNamespace = 'vanilla-extract-css-ns';
 
 export const VanillaCssPlugin: BunPlugin = {
-    name: "VanillaCss",
+    name: 'VanillaCss',
 
     setup(build) {
-        build.onStart(() => {
-            console.log("Bundle started!");
+        build.onResolve({ filter: virtualCssFileFilter }, args => {
+            return {
+                path: args.path,
+                namespace: vanillaCssNamespace,
+            };
         });
+
+        build.onLoad({ filter: /.*/, namespace: vanillaCssNamespace }, async ({ path }) => {
+
+            let { source, fileName } = await getSourceFromVirtualCssFile(path);
+
+            const rootDir = process.cwd();
+
+            const resolveDir = dirname(join(rootDir, fileName));
+
+            return {
+                contents: source,
+                loader: 'css',
+                resolveDir,
+            };
+        });
+
+         
     },
 };
 
